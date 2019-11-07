a5dd857f8ac3fd0647e0b399e5e1af61211f5b06
Tobias <Tobbe3108@gmail.com>
2019-11-07 12:43:50 +0100

    Initial commit

---
+++ PatchPanic.DAL/SpikeContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace PatchPanic.DAL
+{
+    class SpikeContext : DbContext
+    {
+        public SpikeContext() : base() { }
+
+        public SpikeContext(DbContextOptions<SpikeContext> options) : base(options) { }
+
+        public DbSet<SpikeModel> SpikeModels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["DevOpsCon"].ConnectionString);
+            optionsBuilder.UseSqlServer("Server = tcp:tobbeware.database.windows.net,1433; Initial Catalog = TobbeWare; Persist Security Info = False; User ID = Tobbe; Password = 79IufLyBiMsn28; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+        }
+    }
+}
